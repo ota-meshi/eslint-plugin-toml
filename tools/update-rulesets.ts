bf9a6a49be3ec02b2be8b8629ab0509751e1aa5e
Logicer <logicer@logicer.cc>
2024-04-01 14:45:14 +1100

    refine flat config types

--- tools/update-rulesets.ts
+++ tools/update-rulesets.ts
@@ -63,6 +63,7 @@ for (const rec of ["recommended", "standard"] as const) {
  * This file has been automatically generated,
  * in order to update its content execute "npm run update"
  */
+import type { Linter } from "eslint";
 import base from './base';
 export default [
   ...base,
@@ -83,7 +84,7 @@ export default [
         .join(",\n")}
     },
   }
-]
+] satisfies Linter.FlatConfig[]
 `;
 
   const filePath = path.resolve(__dirname, FLAT_RULESET_NAME[rec]);
