d3c2bfa3d2205be00acd8dd4102589ba99246196
Yosuke Ota <otameshiyo23@gmail.com>
2024-04-01 18:53:58 +0900

    Revert "Revert "refine flat config types (#204)" (#205)"
    This reverts commit e090c87661864f84b6ba28df8c08068bcfcdcf1e.

--- src/configs/flat/standard.ts
+++ src/configs/flat/standard.ts
@@ -1,6 +1,7 @@
 // IMPORTANT!
 // This file has been automatically generated,
 // in order to update its content execute "npm run update"
+import type { Linter } from "eslint";
 import base from "./base";
 export default [
   ...base,
@@ -28,4 +29,4 @@ export default [
       "toml/vue-custom-block/no-parsing-error": "error",
     },
   },
-];
+] satisfies Linter.FlatConfig[];
