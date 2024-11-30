ff25f1dd36d590f3c89a1e6e65d455048a6cd93c
yosuke ota <otameshiyo23@gmail.com>
2024-11-30 17:18:18 +0900

    test: add test case for config

--- tests/src/eslint-plugin.ts
+++ tests/src/eslint-plugin.ts
@@ -1,25 +1,44 @@
 import path from "path";
 import assert from "assert";
-import { getLegacyESLint } from "eslint-compat-utils/eslint";
+import { getLegacyESLint, getESLint } from "eslint-compat-utils/eslint";
 import plugin from "../../src/index";
-
-// eslint-disable-next-line @typescript-eslint/naming-convention -- Class name
-const ESLint = getLegacyESLint();
+import { setPlugin } from "../fixtures/integrations/eslint-plugin/plugin-store.cjs";
 
 // -----------------------------------------------------------------------------
 // Tests
 // -----------------------------------------------------------------------------
 
-const TEST_CWD = path.join(__dirname, "../fixtures/integrations/eslint-plugin");
+setPlugin(plugin);
 
 describe("Integration with eslint-plugin-toml", () => {
-  it("should lint without errors", async () => {
+  it("should lint without errors (legacy)", async () => {
+    // eslint-disable-next-line @typescript-eslint/naming-convention -- Class name
+    const ESLint = getLegacyESLint();
     const engine = new ESLint({
-      cwd: TEST_CWD,
+      cwd: path.join(
+        __dirname,
+        "../fixtures/integrations/eslint-plugin/legacy-test01",
+      ),
       extensions: [".js", ".toml"],
       plugins: { "eslint-plugin-toml": plugin as any },
     });
-    const results = await engine.lintFiles(["test01/src"]);
+    const results = await engine.lintFiles(["src"]);
+    assert.strictEqual(results.length, 2);
+    assert.strictEqual(
+      results.reduce((s, a) => s + a.errorCount, 0),
+      0,
+    );
+  });
+  it("should lint without errors", async () => {
+    // eslint-disable-next-line @typescript-eslint/naming-convention -- Class name
+    const ESLint = getESLint();
+    const engine = new ESLint({
+      cwd: path.join(
+        __dirname,
+        "../fixtures/integrations/eslint-plugin/test01",
+      ),
+    });
+    const results = await engine.lintFiles(["src"]);
     assert.strictEqual(results.length, 2);
     assert.strictEqual(
       results.reduce((s, a) => s + a.errorCount, 0),
