a6c256e08093082230d30de3dae27823772dcff1
yosuke ota <otameshiyo23@gmail.com>
2026-01-10 19:22:28 +0900

    chore: fx script

--- tools/lib/changesets-util.ts
+++ tools/lib/changesets-util.ts
@@ -1,9 +1,10 @@
 import getReleasePlan from "@changesets/get-release-plan";
 import path from "path";
 
+const dirname = import.meta.dirname;
 /** Get new version string from changesets */
 export async function getNewVersion(): Promise<string> {
-  const releasePlan = await getReleasePlan(path.resolve(__dirname, "../.."));
+  const releasePlan = await getReleasePlan(path.resolve(dirname, "../.."));
 
   return releasePlan.releases.find(({ name }) => name === "eslint-plugin-toml")!
     .newVersion;
