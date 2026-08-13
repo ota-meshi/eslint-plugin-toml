3ba02bf0d99d0a686f3e0307be4372a9a13823ba
yosuke ota <otameshiyo23@gmail.com>
2026-08-13 12:13:25 +0900

    fix changesets v3 release plan dependency

--- tools/lib/changesets-util.ts
+++ tools/lib/changesets-util.ts
@@ -8,5 +8,5 @@ export async function getNewVersion(): Promise<string> {
   const releasePlan = await getReleasePlan(path.resolve(dirname, "../.."));
 
   return releasePlan.releases.find(({ name }) => name === "eslint-plugin-toml")!
-    .newVersion;
+    .newVersion!;
 }
